bdcf3bc17fce1b5c064ce328dd4a8a0f157250fc
nicoCle <nico.coronelle@gmail.com>
2022-04-07 20:57:10 -0300

    add apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://platzi-graphql.herokuapp.com/graphql",
+  cache: new InMemoryCache(),
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <ChakraProvider>
-        <Component {...pageProps} />;
-      </ChakraProvider>
+      <ApolloProvider client={client}>
+        <ChakraProvider>
+          <Component {...pageProps} />;
+        </ChakraProvider>
+      </ApolloProvider>
     </>
   );
 };
